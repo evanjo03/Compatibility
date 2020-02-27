04a692d0aeaa77d06bfa9173af58f07353ca8e4e
zach <stekza01@luther.edu>
2020-02-26 20:59:23 -0600

    models

---
+++ Compatibility/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Compatibility
+{
+	public class Game
+	{
+		public Guid Id { get;}
+		public string EasyId { get; set; }
+		public List<Team> Teams { get; set; }
+		public List<Image> Images { get; set; }
+		public Player Host { get; set; }
+
+		public Game(string easyId, Player host)
+		{
+			Id = Guid.NewGuid();
+			EasyId = easyId;
+			Teams = new List<Team>();
+			Images = new List<Image>();
+			Host = host;
+
+		}
+	}
+}
---
+++ Compatibility/Team.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Compatibility
+{
+	public class Team
+	{
+		public Guid Id { get; }
+		public string Name { get; set; }
+		public List<Player> Teammates { get; set; }
+		public int Points { get; set; }
+		public int NumRankings { get; set; }
+
+
+		public Team(string name)
+		{
+			Id = Guid.NewGuid();
+			Name = name;
+		}
+
+		public void addPlayer(Player player)
+        {
+			Teammates.Add(player);
+        }
+
+		public void removePlayer(Player player)
+		{
+			Teammates.Remove(player);
+		}
+
+		public void AddPoints(int val)
+        {
+			Points += val;
+        }
+
+	}
+
+
+}
