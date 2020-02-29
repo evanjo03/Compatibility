f1e102c338c8a2368fd31ce9494fc250b9ef5f61
John Evans <john.wes.evans@gmail.com>
2020-02-29 17:22:09 -0600

    login completed

--- core-FE/src/core/header/header.component.ts
+++ core-FE/src/core/header/header.component.ts
@@ -1,31 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
 import { MenuItem } from 'primeng/api';
+import { Constants } from 'src/constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   items: MenuItem[];
 
   ngOnInit() {
     this.items = [
-      {label: 'Help', icon: 'fa fa-info-circle'},
-      {label: 'Sign-Out', icon: 'fa fa-sign-out'},
+      {label: Constants.help, icon: 'fa fa-info-circle', command: (event) => this.handleMenuItemClick(event)},
+      {label: Constants.signOut, icon: 'fa fa-sign-out', command: (event) => this.handleMenuItemClick(event)},
   ];
   }
 
   onLogoClick() {
-    alert('todo - reroute to home');
+    this.router.navigateByUrl('/');
   }
 
-  onMenuClick() {
-    alert('todo - open menu');
+  handleMenuItemClick(event) {
+    if (event.item.label === 'Help') {
+      window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    } else if (event.item.label === Constants.signOut) {
+      this.router.navigateByUrl('/');
+    }
   }
+
 
 }
--- core-FE/src/core/main/main.component.ts
+++ core-FE/src/core/main/main.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn: boolean = false;
 
   ngOnInit() {
   }
